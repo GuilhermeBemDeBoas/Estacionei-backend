bd07cb2327bc5822ac9ee6ce242060b3ee08acb8
Daniel Dantas <dantas.vieira98@hotmail.com>
2019-12-03 20:37:43 -0300

    inclusão recibos

--- src/main/java/com/mackenzie/estacionei/controller/RecibosController.java
+++ src/main/java/com/mackenzie/estacionei/controller/RecibosController.java
@@ -1,8 +1,36 @@
 package com.mackenzie.estacionei.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import com.mackenzie.estacionei.entity.Recibos;
+import com.mackenzie.estacionei.repository.ReciboRepository;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
+@RequestMapping("/recibos")
 public class RecibosController {
-
+    
+    private float total=0;
+    @Autowired
+    private ReciboRepository recibosRepository;
+    
+    @GetMapping
+    public float fechamentoDiario(@RequestParam LocalDate data){
+        if(data == null) {
+           // TODO throw Exception;
+        } else {
+            List<Recibos> recibos = recibosRepository.findByData(data);
+            for (int i=0; recibos.size()<i;i++) {
+                total+=recibos.indexOf(i);
+                
+            }
+        }
+        return total;
+    }
 }
--- src/main/java/com/mackenzie/estacionei/controller/dto/RecibosDTO.java
+++ src/main/java/com/mackenzie/estacionei/controller/dto/RecibosDTO.java
@@ -1,5 +1,45 @@
 package com.mackenzie.estacionei.controller.dto;
 
-public class RecibosDTO {
+import com.mackenzie.estacionei.entity.Recibos;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class RecibosDTO {
+    private Long idRecibo;
+    private LocalDateTime dataHora;
+    private Integer valorPago;
+
+    public RecibosDTO(Recibos recibos) {
+        this.idRecibo = idRecibo;
+        this.dataHora = dataHora;
+        this.valorPago = valorPago;
+    }
+    
+    public RecibosDTO() {
+        
+    }
+
+    public Long getIdRecibo() {
+	return idRecibo;
+    }
+    public void setIdRecibo(Long idRecibo) {
+        this.idRecibo = idRecibo;
+    }
+    public LocalDateTime getDataHora() {
+	return dataHora;
+    }
+    public void setDataHora(LocalDateTime dataHora) {
+	this.dataHora = dataHora;
+    }
+    public Integer getValorPago() {
+	return valorPago;
+    }
+    public void setValorPago(Integer valorPago) {
+	this.valorPago = valorPago;
+    }
+    
+     public static List<RecibosDTO> parse (List<Recibos> recibos){
+        return recibos.stream().map(RecibosDTO::new).collect(Collectors.toList());
+    }
 }
