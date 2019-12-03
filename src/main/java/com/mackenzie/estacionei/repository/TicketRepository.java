54dd0250dddf0720689233a9a760ba89b606b88d
Gamarote <gamarote@hotmail.com>
2019-12-03 19:25:35 -0300

    Added between date range query

--- src/main/java/com/mackenzie/estacionei/repository/TicketRepository.java
+++ src/main/java/com/mackenzie/estacionei/repository/TicketRepository.java
@@ -2,6 +2,14 @@ package com.mackenzie.estacionei.repository;
 
 import com.mackenzie.estacionei.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    @Query("SELECT t FROM Ticket t WHERE t.date BETWEEN :startDate AND :endDate")
+    List<Ticket> getByDateRange(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
 }
